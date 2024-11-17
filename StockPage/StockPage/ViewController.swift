88e1a4de90413e00c660de91e8e0585c809b70e5
utkarshraj <utkarshraj@urbancompany.com>
2024-11-17 15:07:56 +0530

    Initial commit

---
+++ StockPage/StockPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StockPage
+//
+//  Created by Utkarsh Raj on 17/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
